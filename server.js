b46780e8c0aa18cf6604579b45b18aca49273014
FebliRamadhan <ramadhan.febli11@gmail.com>
2019-05-02 10:38:10 +0700

    add docker

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
-const redis = require('redis');
-const redisClient = redis.createClient({host : "127.0.0.1", port : 6379});
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -15,27 +13,12 @@ const mongoose = require('mongoose');
 
 mongoose.Promise;
 
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-    }).then(() => {
-        console.log("Successfully connected to the database");    
-    }).catch(err => {
-        console.log('Could not connect to the database. Exiting now...', err);
-        process.exit();
-})
-redisClient.on('error', () => {
-    console.log("Error in redis");
-    
-});
-
 app.get('/', (req, res) => {
     res.json({"message" : "Welcome to REST API Mahasiswa"});
 });
 
 require('./app/routes/mahasiswa.routes.js')(app);
 
-app.listen(3002, () => {
-    console.log("Server is listening port 3002");
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening at port ${process.env.PORT}`);
 });
-
-
